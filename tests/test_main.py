15808f5f0b71ed1b29a5ee44d09cdc7abfb88070
lukas_baumgarten <lukas.baumgarten@xvalue.de>
2024-08-29 14:40:35 +0200

    update test style and linting

--- tests/test_main.py
+++ tests/test_main.py
@@ -9,18 +9,6 @@ client = TestClient(main.app)  # Create a TestClient instance
 # Define a list of dictionaries for different test cases
 test_cases = [
     {"path": "/", "expected_status": 200, "expected_text": "Welcome to Air Pollution Data Viewer"},
-    {
-        "path": "/data/Germany/1750/2020/stats",
-        "expected_status": 200,
-        "expected_text": """    <p>Statistics for all parameters for Germany from 1750 to 2020:</p>
-
-        <h3>Nitrogen Oxide (NOx)</h3>
-        <ul>
-            <li>Mean: 835946.1614044248</li>
-            <li>Median: 671570.345</li>
-            <li>Standard Deviation: 852202.1476415253</li>
-        </ul> """,
-    },
 ]
 
 
